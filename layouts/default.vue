578271c060c608bbf7b85717f288676df8c14dec
darkside <andrey.garbuz@gmail.com>
2017-07-16 22:39:26 +0300

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app dark>
+    <main>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <h2>Carousel</h2>
+      <v-carousel>
+        <v-carousel-item v-for="(item,i) in items" :src="item.src" :key="i"></v-carousel-item>
+      </v-carousel>
+
+      <h2>Card Media</h2>
+      <v-card>
+        <v-card-media src="https://dummyimage.com/600x400/de33de/fff" height="600px"></v-card-media>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        { src: 'https://dummyimage.com/600x400/000/fff' },
+        { src: 'https://dummyimage.com/600x400/fff/000' }
+      ]
+    }
+  }
+}
+</script>
